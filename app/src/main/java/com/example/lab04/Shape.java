81f02e2d7c84bba2be47ac89420bdfa94437f99a
Thet Hnin Hsu <ditracy10@gamil.com>
2026-01-28 19:13:23 -0700

    updated circle shape

--- app/src/main/java/com/example/lab04/Shape.java
+++ app/src/main/java/com/example/lab04/Shape.java
@@ -4,7 +4,6 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
-    protected String color = "blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
