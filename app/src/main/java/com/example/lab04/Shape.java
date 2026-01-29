0e11e6b4e4f393a16d6eb8ffdfc1a887695fe01a
isr808 <Ishan.raj808@gmail.com>
2026-01-28 18:17:57 -0700

    added abstract shape class

---
+++ app/src/main/java/com/example/lab04/Shape.java
@@ -0,0 +1,13 @@
+package com.example.lab04;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
