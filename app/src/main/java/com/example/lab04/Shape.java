3d0bdc2a23301c3eef80337317516da3e5e3f15b
Thet Hnin Hsu <ditracy10@gamil.com>
2026-01-28 19:18:07 -0700

    Updated cirlce shape & color

--- app/src/main/java/com/example/lab04/Shape.java
+++ app/src/main/java/com/example/lab04/Shape.java
@@ -3,7 +3,7 @@ package com.example.lab04;
 public abstract class Shape {
     protected int x;
     protected int y;
-
+    protected String color = "blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
